598fbda13a2d6c6a64bed93a273d7866a791ede2
CJQuintero61 <cjquintero61@gmail.com>
2025-04-21 14:49:07 -0500

    learned stuff

---
+++ DataTypes/SpecificTypes.py
@@ -0,0 +1,12 @@
+# You can specify data types
+#
+# author: CJ Quintero
+# last updated: 04/21/2025
+
+firstName = str("CJ")
+age = int(20)
+myNumbersList = list([1,2, 3])
+
+print(myNumbersList)
+print(f"Hello {firstName}")
+print(f"I am {age}")
