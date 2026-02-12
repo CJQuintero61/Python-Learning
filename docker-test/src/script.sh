2ee04f99a46682fc6e134dba279d95da8930e13d
CJQuintero61 <cjquintero61@gmail.com>
2026-02-12 12:23:10 -0600

    added connections

---
+++ docker-test/src/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This shell script is an example to show how
+# docker containers can run a shell script on
+# any device even if they don't normally support 
+# shell. For example, Windows can't run shell
+# scripts, but things like docker or WSL can 
+# work around this issue. The container can
+# instead run the shell script without Windows
+# throwing an error for a shell script.
+
+echo "Running the shell script!"
