837d0d91c07b7b65331d3b6423ff32e35a1a846c
CJQuintero61 <cjquintero61@gmail.com>
2026-02-12 12:31:53 -0600

    comment fix

--- docker-test/src/script.sh
+++ docker-test/src/script.sh
@@ -10,7 +10,7 @@
 # throwing an error for a shell script.
 
 # to run:
-# 1. buildl containers with 'docker compose up --build -d'
+# 1. build containers with 'docker compose up --build -d'
 # 2. run the script with 'docker exec my-python-container /bin/bash ./src/script.sh'
 # 3. if you make any changes, rebuild with step 1 and re run with step 2
 # 4. when finished, remove the containers with 'docker compose down'
